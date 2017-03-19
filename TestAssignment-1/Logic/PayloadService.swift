ba1f1099f540589ce1b72219ac2ff1b5382b69b2
Dudarenko Ilya <statusgman@gmail.com>
2017-03-19 16:57:12 +0300

    Added core data stack and network service

---
+++ TestAssignment-1/Logic/PayloadService.swift
@@ -0,0 +1,22 @@
+//
+//  PayloadService.swift
+//  TestAssignment-1
+//
+//  Created by Dudarenko Ilya on 19/03/2017.
+//  Copyright © 2017 Dudarenko Ilya. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class PayloadService {
+    func fetchPayload(with completion:(Error, [String])->()) {
+        
+    }
+    
+    func fetchDetailsForPayload(with identifier: String,
+                                andCompletion completion:(Error, [String])->())
+    {
+        
+    }
+}
---
+++ TestAssignment-1/UI/NewsListViewController.swift
@@ -0,0 +1,64 @@
+//
+//  NewsListViewController.swift
+//  
+//
+//  Created by Dudarenko Ilya on 19/03/2017.
+//
+//
+
+import UIKit
+import Alamofire
+
+class NewsListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+//        cell.textLabel?.text =
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
