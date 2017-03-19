5adbde91204c43793f632d8c2ba28fd084acd2a4
Dudarenko Ilya <statusgman@gmail.com>
2017-03-19 14:29:12 +0300

    Project added, pods added

---
+++ Pods/Target Support Files/Pods-TestAssignment-1/Pods-TestAssignment-1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestAssignment_1 : NSObject
+@end
+@implementation PodsDummy_Pods_TestAssignment_1
+@end
---
+++ TestAssignment-1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestAssignment-1
+//
+//  Created by Dudarenko Ilya on 19/03/2017.
+//  Copyright © 2017 Dudarenko Ilya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
