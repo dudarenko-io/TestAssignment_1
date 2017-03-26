9ef5618452ec6bc50b6c331c18f0753863546363
Dudarenko Ilya <statusgman@gmail.com>
2017-03-26 19:58:59 +0300

    finished with service layer

--- TestAssignment-1/Logic/PayloadService.swift
+++ TestAssignment-1/Logic/PayloadService.swift
@@ -10,8 +10,15 @@ import Foundation
 import Alamofire
 
 class PayloadService {
+    
+    fileprivate struct URLs {
+        static let newsList = "https://api.tinkoff.ru/v1/news"
+        static let newsDetail = "https://api.tinkoff.ru/v1/news_content"
+    }
+    
     func fetchPayload(with completion:@escaping (Error?, [String])->()) {
-        Alamofire.request("https://api.tinkoff.ru/v1/news").responseJSON { response in
+        Alamofire.request(URLs.newsList)
+            .responseJSON { response in
             if let error = response.error {
                 completion(error, [String]())
                 return
@@ -44,8 +51,26 @@ class PayloadService {
     }
     
     func fetchDetailsForPayload(with identifier: String,
-                                andCompletion completion:@escaping (Error?, [String])->())
+                                andCompletion completion:@escaping (Error?, String)->())
     {
-        // https://api.tinkoff.ru/v1/news_content?id={payload[i].id}
+        let parameters: Parameters = ["id": identifier]
+        Alamofire.request(URLs.newsDetail,
+                          method: .get,
+                          parameters: parameters)
+            .responseJSON { (response) in
+            if let error = response.error {
+                completion(error, String())
+                return
+            }
+            
+            if let JSON = response.result.value as? [String:Any]{
+                if let payload = JSON["payload"] as? [String:Any] {
+                    if let title = payload["content"] as? String {
+                        completion(nil, title)
+                    }
+                }
+
+            }
+        }
     }
 }
